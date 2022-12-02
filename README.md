a2fa2a7399014578558ee33366d92bb7554a788c
feyza <feyzaeksen@gmail.com>
2022-12-02 19:04:10 +0300

    hash get

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ export FLASK_APP="application.py"
 flask run
 ```
 
-curl -i -H "Content-Type: application/json" -X POST -d '{"message":"feyza"}' http://localhost:5000/messages
+```
 echo -n "feyza" | shasum -a 256
 
-curl -i -H "Content-Type: application/json" -X POST -d '{"message":"feyza"}' http://flaskexample-env.eba-fisvhbir.us-east-1.elasticbeanstalk.com/messages
+curl -X POST -H "Content-Type: application/json" -d '{ "message":"feyza"}' http://flaskexample-env.eba-fisvhbir.us-east-1.elasticbeanstalk.com/messages
+
+curl -X GET "localhost:5000/messages?hash=6e8c90bed1ab401092001331be8803c89c5ca2d0283b7d8cbbb5dc19bf483cef"
+```
--- application.py
+++ application.py
@@ -5,13 +5,22 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return 'Hello there good sir.'
+    return "Welcome!"
 
 @application.route("/messages", methods=["POST"])
-def messages():
+def messages(): # TODO: post_message
     message = request.json["message"]
     if not message:
         return "no message", 401
 
     hash_value = hashlib.sha256(message.encode('utf-8')).hexdigest()
     return hash_value, 200
+
+@application.route("/messages", methods=["GET"])
+def get_message():
+    hash_value = request.args.get("hash")
+
+    if hash_value == "6e8c90bed1ab401092001331be8803c89c5ca2d0283b7d8cbbb5dc19bf483cef": #TODO: change it
+        return "feyza", 200 #TODO: change it
+    else:
+        return "hash not found", 404
