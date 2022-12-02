38680406f82e8621a58a2a0ea69b7133f9b8e4bb
feyza <feyzaeksen@gmail.com>
2022-12-02 18:21:46 +0300

    post

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ source .venv/bin/activate
 export FLASK_APP="application.py"
 flask run
 ```
+
+curl -i -H "Content-Type: application/json" -X POST -d '{"message":"feyza"}' http://localhost:5000/messages
+echo -n "feyza" | shasum -a 256
--- application.py
+++ application.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+import hashlib
+
 application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
     return 'Hello there good sir.'
+
+@application.route("/messages", methods=["POST"])
+def messages():
+    message = request.json["message"]
+    if not message:
+        return "no message", 401
+
+    hash_value = hashlib.sha256(message.encode('utf-8')).hexdigest()
+    return hash_value, 200
