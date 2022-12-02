7c5a1a6d5fe27298a534eae24a7582227076d5b4
feyza <feyzaeksen@gmail.com>
2022-12-02 19:33:32 +0300

    code complete

--- README.md
+++ README.md
@@ -7,7 +7,10 @@ flask run
 ```
 echo -n "feyza" | shasum -a 256
 
-curl -X POST -H "Content-Type: application/json" -d '{ "message":"feyza"}' http://flaskexample-env.eba-fisvhbir.us-east-1.elasticbeanstalk.com/messages
+curl -X POST -H "Content-Type: application/json" -d '{ "message":"<your_message>"}' http://flaskexample-env.eba-fisvhbir.us-east-1.elasticbeanstalk.com/messages
+curl -X GET "http://flaskexample-env.eba-fisvhbir.us-east-1.elasticbeanstalk.com/messages?hash=<your_hash>"
 
-curl -X GET "localhost:5000/messages?hash=6e8c90bed1ab401092001331be8803c89c5ca2d0283b7d8cbbb5dc19bf483cef"
+Example:
+curl -X POST -H "Content-Type: application/json" -d '{ "message":"feyza"}' http://flaskexample-env.eba-fisvhbir.us-east-1.elasticbeanstalk.com/messages
+curl -X GET "http://flaskexample-env.eba-fisvhbir.us-east-1.elasticbeanstalk.com/messages?hash=6e8c90bed1ab401092001331be8803c89c5ca2d0283b7d8cbbb5dc19bf483cef"
 ```
--- application.py
+++ application.py
@@ -3,24 +3,31 @@ import hashlib
 
 application = Flask(__name__)
 
+memo = {}
+
 @application.route('/')
 def hello_world():
     return "Welcome!"
 
 @application.route("/messages", methods=["POST"])
-def messages(): # TODO: post_message
+def post_message():
     message = request.json["message"]
     if not message:
         return "no message", 401
 
     hash_value = hashlib.sha256(message.encode('utf-8')).hexdigest()
+    memo[hash_value] = message
     return hash_value, 200
 
 @application.route("/messages", methods=["GET"])
 def get_message():
     hash_value = request.args.get("hash")
 
-    if hash_value == "6e8c90bed1ab401092001331be8803c89c5ca2d0283b7d8cbbb5dc19bf483cef": #TODO: change it
-        return "feyza", 200 #TODO: change it
+    if hash_value in memo:
+        return memo[hash_value], 200
     else:
         return "hash not found", 404
+
+@application.route("/list")
+def list_memo():
+    return memo
